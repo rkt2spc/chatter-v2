acf87f05a333569de10310ef03104145777869ca
Tuan Nguyen <nmtuan.dev@gmail.com>
2017-04-05 16:59:05 +0700

    Change 4 words comment...

--- chatter.portal/server.js
+++ chatter.portal/server.js
@@ -25,7 +25,7 @@ app.use(express.static(path.resolve(__dirname, 'public')));
 app.use((err, req, res, next) => logger.error(err));
 
 //========================================================================================================
-// Http Server
+// Startup
 var httpServer = http.createServer(app);
 httpServer
     .listen(serverSettings.PORT)
